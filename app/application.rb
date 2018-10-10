3bfa0ebee3dfcd7aa02af5f8d2fa1e4425459cd1
Justine Bricknell <justinegayton@gmail.com>
2018-10-10 23:52:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,5 +29,20 @@ class Application
     end
   end
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Your cart is empty"
+    end
+    resp.finish
+  end
 
 end
