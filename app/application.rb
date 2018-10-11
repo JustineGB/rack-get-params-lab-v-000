a49a68bb178c385b42054e994ca7ca45863a02a5
Justine Bricknell <justinegayton@gmail.com>
2018-10-11 00:06:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,12 +10,30 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      end
+        end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+        if @@cart.empty? 
+            resp.write "Your cart is empty"
+        else 
+            @@cart.each do |item|
+            resp.write "#{item}\n"
+          end 
+      
+    elsif req.path.match(/add/)
+      
+     @@items.find
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+      item = search_term 
+      if @@items.include?(item)
+        @@cart << item 
+        
     else
-      resp.write "Path Not Found"
+      resp.write "Your cart is empty"
     end
 
     resp.finish
@@ -29,28 +47,14 @@ class Application
     end
   end
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    if req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Your cart is empty"
-    end
+  def add_item(item)
+    
+
+
+  
+    
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@cart.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-
+  
 end
